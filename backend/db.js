89fff5d79eba6a65c26831a998a1f21af0d99a04
Vinay2334 <98982309+Vinay2334@users.noreply.github.com>
2022-10-09 19:42:37 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-const mongooseURI=('mongodb+srv://Vinay:9968393810ab@cluster0.blhljtt.mongodb.net/inotebook?retryWrites=true&w=majority');
+const mongooseURI=('');
 const connectToMongo=()=>{
     mongoose.connect(mongooseURI,()=>{
         console.log("connected to mongo")
